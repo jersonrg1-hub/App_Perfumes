f8e581fd795ce9b453453c52044a4bc03ee4f52e
jerson.rg1-hub <jerson.rg1@gmail.com>
2026-03-19 13:27:34 -0500

    nueva clave y credenciales actualizadas

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ st.set_page_config(page_title="Perfumes 🌸", page_icon="🌸", layout="centere
 
 @st.cache_data
 def cargar_catalogo():
-    # ✅ Esta usa los Secrets de Streamlit Cloud
     creds = Credentials.from_service_account_info(
         st.secrets["gcp_service_account"], scopes=SCOPES
     )
